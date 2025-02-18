c6fe03bccb07748cf72e8ba6c555319a26270192
Mark Bailey <MCBaileyGitHub@gmail.com>
2025-02-18 10:59:43 -0500

    GMCleanAssembly is working:
    Need to implement comments.
    Need to pass all mypy checks.

--- GMCleanAssembly.py
+++ GMCleanAssembly.py
@@ -20,37 +20,14 @@ def display_part() -> NXOpen.Part:
 
 # Stopwatch stopwatch = Stopwatch.StartNew();
 
-# try
-# {
-#     // remove castings first
-#     // MATERIAL == '2500, 3500'
-#     // whack parameters
-#     // remove fasteners
-#     // all lines
-#     // notes
-#     // drawings
-#     // all suppressed components
-#     // in theory there should only be one body solid body on layer 1.
-#     // assembly needs to be fully loaded, try to load, else tell the user they need to fully load. Show them the assembly path to the unloaded components.
-#     // alert if part has more than one solid body on layer 1 at the end
-
-
-#     // need to delete the udo
-#     // also need to delete the all groups you can find, expressions and stuff
-#     // part cleanup the whole assembly.
-#     session_.SetUndoMark(Session.MarkVisibility.Visible, "GMSeal");
-# session = NXOpen.Session.GetSession()
-
-
-# def list_files_recursive(directory):
-#     for root, dirs, files in os.walk(directory):
-#         for file in files:
-#             print(os.path.join(root, file))
-
-
 def get_all_descendants(component):
     descendants = []
+    # print_('hdhdhdh')
 
+    if component is None:
+        # print_('none')
+        return descendants
+    
     # Get children of the current component
     children = component.GetChildren()
 
@@ -64,22 +41,28 @@ def get_all_descendants(component):
 
 
 def DescendantParts(part):
-    parts = {}
+    __parts = {}
+    __parts[part.Leaf] =part
     for component in get_all_descendants(part.ComponentAssembly.RootComponent):
         if isinstance(component.Prototype, NXOpen.Part):
-            if component.DisplayName not in parts.keys():
-                parts[component.DisplayName] = component.Prototype
-    return list(parts.values())
+            if component.DisplayName not in __parts.keys():
+                __parts[component.DisplayName] = component.Prototype
+    return list(__parts.values())
+
+def delete_objects(objects)->None:
+    session().UpdateManager.ClearDeleteList()
+    undo = session().SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "DELETE")
+    session().UpdateManager.AddObjectsToDeleteList(objects)
+    session().UpdateManager.DoUpdate(undo)
 
 
-# undo = session.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "DELETE")
-# session.UpdateManager.AddObjectsToDeleteList(session.Parts.Display.ComponentAssembly.RootComponent.GetChildren())
-
-# session.UpdateManager.DoUpdate(undo)
 
 
 # def main():
+print_("in here")
 session().SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "GMCleanAssembly" )
+
+original_display = session().Parts.Display
 # print_('helllo world')
 fasteners = {}
 
@@ -98,16 +81,13 @@ parts = list(filter(lambda p: p.Leaf not in fasteners, parts))
 parts = list(filter(lambda p: 'layout'not in p.Leaf.lower()  , parts))
 parts = list(filter(lambda p: 'strip'not in p.Leaf.lower()  , parts))
 
-
 #     using (session_.__UsingDisplayPartReset())
 #     using (session_.__UsingSuppressDisplay())
 #         for (int i = 0; i < parts.Length; i++)
 
+# print_(len(parts))
 
-
-# for index, value in enumerate(parts):
-
-
+for index, value in enumerate(parts):
 
 #             try
 #             {
@@ -115,125 +95,107 @@ parts = list(filter(lambda p: 'strip'not in p.Leaf.lower()  , parts))
 #                 prompt($"({i + 1} - {parts.Length}) -> {part.Leaf}");
 
 
+    session().Parts.SetDisplay(value, False, False)
 
-    # session().Parts.SetDisplay(value, False, False)
-    
+    suppresed_features = list(filter(lambda f:not f.Suppressed, display_part().Features))
 
+    if len(suppresed_features) > 0:
+        # session_.__DeleteObjects(suppresed_features);
+        pass
 
+    # display = session.Parts.Display
+    removeBuilder = display_part().Features.CreateRemoveParametersBuilder()
+    removeBuilder.Objects.Add(list(display_part().Bodies))
+    removeBuilder.Objects.Add(list(display_part().Curves))
+    removeBuilder.Objects.Add(list(display_part().Points))
+    removeBuilder.Objects.Add(list(display_part().Datums))
+    removeBuilder.Objects.Add(list(display_part().CoordinateSystems))
+    if removeBuilder.Objects.Size > 0:
+        removeBuilder.Commit()
+    removeBuilder.Destroy()
 
-suppresed_features = list(filter(lambda f:not f.Suppressed, display_part().Features))
-print_(len(suppresed_features))
-                    
+    non_layer_1_bodies = list(filter(lambda b:b.Layer != 1, list(display_part().Bodies)))
 
-#                 if (suppresed_features.Length > 0)
-#                     session_.__DeleteObjects(suppresed_features);
+    if len(non_layer_1_bodies) > 0:
+        delete_objects(non_layer_1_bodies)
 
+    Curves = list(display_part().Curves)
 
-# display = session.Parts.Display
-# removeBuilder = display.Features.CreateRemoveParametersBuilder()
-# removeBuilder.Objects.Add(display.Bodies.ToArray())
-# removeBuilder.Objects.Add(display.Curves.ToArray())
-# removeBuilder.Objects.Add(display.Points.ToArray())
-# removeBuilder.Objects.Add(display.Datums.ToArray())
-# removeBuilder.Objects.Add(display.CoordinateSystems.ToArray())
+    if len(Curves)> 0:
+        delete_objects(Curves)
 
-#     if len(removeBuilder.Objects) > 0:
-#         removeBuilder.Commit()
-# removeBuilder.Destory()
+    Datums = list(display_part().Datums)
 
-#                 Body[] non_layer_1_bodies = __display_part_.Bodies
-#                     .ToArray()
-#                     .Where(body => body.Layer != 1)
-#                     .ToArray();
+    if len(Datums)> 0:
+        delete_objects(Datums)
 
-#                 if (non_layer_1_bodies.Length > 0)
-#                     session_.__DeleteObjects(non_layer_1_bodies);
+    Notes = list(display_part().Notes)
 
+    if len(Notes)> 0:
+        delete_objects(Notes)
 
-#                 session_.__DeleteObjects(__display_part_.Curves.ToArray());
-#                 session_.__DeleteObjects(__display_part_.Datums.ToArray());
-#                 session_.__DeleteObjects(__display_part_.Notes.ToArray());
+    if display_part().ComponentAssembly.RootComponent is not None:
+        for child in list(display_part().ComponentAssembly.RootComponent.GetChildren()):
+            if child.DisplayName in fasteners or child.IsSuppressed:
+                delete_objects([child])
 
-#                 foreach (NXOpen.Assemblies.Component comp in __display_part_.__Children())
-#                     if (fasteners.Contains(comp.DisplayName) || comp.IsSuppressed)
-#                         comp.__Delete();
+    contraints = list(display_part().DisplayedConstraints)
 
-#                 NXOpen.Positioning.DisplayedConstraint[] contraints = __display_part_.DisplayedConstraints.ToArray();
+    if len(contraints)> 0:
+        delete_objects(contraints)
 
-#                 if (contraints.Length > 0)
-#                     session_.__DeleteObjects(contraints);
+    drawings = list(display_part().DrawingSheets)
 
-#                 NXOpen.Drawings.DrawingSheet[] drawings = __display_part_.DrawingSheets.ToArray();
+    if len(drawings)> 0:
+        delete_objects(drawings)
 
-#                 if (drawings.Length > 0)
-#                     session_.__DeleteObjects(drawings);
-
-
-#                 __display_part_.ComponentGroups
-#                     .ToArray()
-#                     .ToList()
-#                     .ForEach(g => g.__Delete());
-#             }
-#             catch (Exception ex)
-#             {
-#                 ex.__PrintException(parts[i].Leaf);
-#             }
-#     prompt("Performing Part Cleanup");
-#     try
-#     {
-#         PartCleanup part_cleanup = session_.NewPartCleanup();
+session().Parts.SetDisplay(original_display, False, False)
+part_cleanup = session().NewPartCleanup()
 
 #         using (part_cleanup)
 #         {
 
-#             part_cleanup.PartsToCleanup = CleanupParts.All;
-#             part_cleanup.CleanupAssemblyConstraints = true;
-#             part_cleanup.CleanupCAMObjects = true;
-#             part_cleanup.CleanupDraftingObjects = true;
-#             part_cleanup.CleanupFeatureData = true;
-#             part_cleanup.CleanupMatingData = true;
-#             part_cleanup.CleanupMotionData = true;
-#             part_cleanup.CleanupPartFamilyData = true;
-#             part_cleanup.CleanupRoutingData = true;
-#             part_cleanup.DeleteBrokenInterpartLinks = true;
-#             part_cleanup.DeleteDuplicateLights = true;
-#             part_cleanup.DeleteInvalidAttributes = true;
-#             part_cleanup.DeleteMaterials = true;
-#             part_cleanup.DeleteSpreadSheetData = true;
-#             part_cleanup.DeleteUnusedExpressions = true;
-#             part_cleanup.DeleteUnusedExtractReferences = true;
-#             part_cleanup.DeleteUnusedFonts = true;
-#             part_cleanup.DeleteUnusedObjects = true;
-#             part_cleanup.DeleteUnusedUnits = true;
-#             part_cleanup.DeleteVisualEditorData = true;
-#             part_cleanup.FixOffplaneSketchCurves = true;
-#             part_cleanup.GroupsToDelete = PartCleanup.DeleteGroups.All;
-#             part_cleanup.ResetComponentDisplay = ResetComponentDisplayAction.RemoveAllChanges;
-#             part_cleanup.TurnOffHighlighting = true;
-#             part_cleanup.DoCleanup();
-#         }
-#     }
-#     catch (Exception ex)
-#     {
-#         ex.__PrintException();
-#     }
-# }
-# catch (Exception ex)
-# {
-#     ex.__PrintException();
-# }
+# for k in dir(NXOpen.PartCleanup.CleanupParts):
+#     print_(k)
 
-# stopwatch.Stop();
 
-# print(stopwatch.Elapsed);
-# class Main_:
-#     @staticmethod
 
-# print()
-# list_files_recursive('G:\\0Library\\Fasteners')
-# print_(os.listdir(,))
+# change this to Components.
+# that way when the program gets here,
+# we should be at the first display part.
+# it will prevent them from cleaning up all open parts
+# and fasteners that might not pertain to this assembly any more
+part_cleanup.PartsToCleanup = NXOpen.PartCleanup.CleanupParts.Components
+part_cleanup.CleanupAssemblyConstraints = True
+part_cleanup.CleanupCAMObjects = True
+part_cleanup.CleanupDraftingObjects = True
+part_cleanup.CleanupFeatureData = True
+part_cleanup.CleanupMatingData = True
+part_cleanup.CleanupMotionData = True
+part_cleanup.CleanupPartFamilyData = True
+part_cleanup.CleanupRoutingData = True
+part_cleanup.DeleteBrokenInterpartLinks = True
+part_cleanup.DeleteDuplicateLights = True
+part_cleanup.DeleteInvalidAttributes = True
+part_cleanup.DeleteMaterials = True
+part_cleanup.DeleteSpreadSheetData = True
+part_cleanup.DeleteUnusedExpressions = True
+part_cleanup.DeleteUnusedExtractReferences = True
+part_cleanup.DeleteUnusedFonts = True
+part_cleanup.DeleteUnusedObjects = True
+part_cleanup.DeleteUnusedUnits = True
+part_cleanup.DeleteVisualEditorData = True
+part_cleanup.FixOffplaneSketchCurves = True
+part_cleanup.GroupsToDelete = NXOpen.PartCleanup.DeleteGroups.All
+part_cleanup.ResetComponentDisplay = NXOpen.PartCleanup.ResetComponentDisplayAction.RemoveAllChanges
+part_cleanup.TurnOffHighlighting = True
+part_cleanup.DoCleanup()
 
-#     print_("madr it in here")
 
-# if __name__ == '__main__':
-#     main()
+# def scale_part(part, scale_factor):
+#     # Get the work part's bodies and scale them
+#     bodies = part.Bodies
+#     for body in bodies:
+#         # Scale each body in the part by the scale factor
+#         body.Transform(NXOpen.Matrix3x3.Identity(), NXOpen.Vector3d.Zero(), scale_factor)
+#         print(f"Scaled body: {body.Name} by factor {scale_factor}")
