4022cafa0a45dbd1720e75ecd9f5b35bbfd388da
Mark Bailey <MCBaileyGitHub@gmail.com>
2025-02-19 11:14:14 -0500

    GmColorLayerAssembly seems to be working

--- GMColorLayerAssembly.py
+++ GMColorLayerAssembly.py
@@ -36,14 +36,14 @@ def select_components() -> List[Component]:
 #     print_(x)
 
 # for x in dir(NXOpen.UF.UFSession.GetUFSession().Ui.GetInputIntegers):
-comp = display_part().ComponentAssembly.RootComponent.GetChildren()[0]
+# comp = display_part().ComponentAssembly.RootComponent.GetChildren()[0]
 
-# print_(comp.DisplayName)
-# print_(comp.Tag)
-# print_(comp)
-xxx = NXOpen.TaggedObjectManager.GetTaggedObject(comp.Tag)
-# print_(repr(xxx))
-ggg = NXOpen.Selection.MaskTriple(63, 0, 0)
+# # print_(comp.DisplayName)
+# # print_(comp.Tag)
+# # print_(comp)
+# xxx = NXOpen.TaggedObjectManager.GetTaggedObject(comp.Tag)
+# # print_(repr(xxx))
+# ggg = NXOpen.Selection.MaskTriple(63, 0, 0)
 
 # print_(ggg)
 
@@ -64,78 +64,85 @@ ggg = NXOpen.Selection.MaskTriple(63, 0, 0)
 
 # k
 
-components = select_components()
-NXOpen.UF.UFSession.GetUFSession().Ui.SetStatus(str(components))
+def __main__(layer:int, color:int)->None:
 
-# the color to change the body
-color = 186
-# the layer to set the first and last solid body on layer 1 in a detail
-layer = 42
+    components = select_components()
+    # NXOpen.UF.UFSession.GetUFSession().Ui.SetStatus(str(components))
 
-original = display_part()
+    # the color to change the body
+    # the layer to set the first and last solid body on layer 1 in a detail
 
-try:
-    theSession = NXOpen.Session.GetSession()
-    # need to convert {components} to a set of parts incase they pick the user picks two components with the same prototype
-    for comp in components:
-        # need to set the {comp} to the {layer} first before you change displayed part
+    original = display_part()
 
-        # foreach selected component, need to prompt the user for a layer, and color
+    try:
+        theSession = NXOpen.Session.GetSession()
+        # need to convert {components} to a set of parts incase they pick the user picks two components with the same prototype
+        for comp in components:
+            # need to set the {comp} to the {layer} first before you change displayed part
 
-        # comp.SetLayerOption(-1)
+            # foreach selected component, need to prompt the user for a layer, and color
 
-        session_.Parts.SetDisplay(comp.Prototype, False, False)
-        solid_body_layer_1 = list(
-            filter(lambda b: b.Layer == 1, list(session_.Parts.Display.Bodies))
-        )
-        if len(solid_body_layer_1) != 1:
-            raise Exception(
-                f"There were {len(solid_body_layer_1)} solid bodies in part {comp.DisplayName}"
+            # comp.SetLayerOption(-1)
+
+            session_.Parts.SetDisplay(comp.Prototype, False, False)
+            solid_body_layer_1 = list(
+                filter(lambda b: b.Layer == 1, list(session_.Parts.Display.Bodies))
             )
-        solid_body_layer_1 = solid_body_layer_1[0]
+            if len(solid_body_layer_1) != 1:
+                raise Exception(
+                    f"There were {len(solid_body_layer_1)} solid bodies in part {comp.DisplayName}"
+                )
+            solid_body_layer_1 = solid_body_layer_1[0]
 
-        displayModification1 = theSession.DisplayManager.NewDisplayModification()
-        displayModification1.ApplyToAllFaces = True
-        # look at this.
-        # if set to true you can probably change the color of the
-        # prototype body and layer from the component level
-        # without ever changing the displayed part.
-        # that way you can always stay at the top level assembly.
-        displayModification1.ApplyToOwningParts = False
-        displayModification1.NewColor = color
-        displayModification1.NewLayer = layer
-        displayModification1.Apply([solid_body_layer_1])
-        theSession.UpdateManager.DoUpdate(
-            session_.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "m")
-        )
-        displayModification1.Dispose()
+            displayModification1 = theSession.DisplayManager.NewDisplayModification()
+            displayModification1.ApplyToAllFaces = True
+            # look at this.
+            # if set to true you can probably change the color of the
+            # prototype body and layer from the component level
+            # without ever changing the displayed part.
+            # that way you can always stay at the top level assembly.
+            displayModification1.ApplyToOwningParts = False
+            displayModification1.NewColor = color
+            displayModification1.NewLayer = layer
+            displayModification1.Apply([solid_body_layer_1])
+            # theSession.UpdateManager.DoUpdate(
+            #     session_.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "m")
+            # )
+            displayModification1.Dispose()
+            print_(solid_body_layer_1.Layer)
+# bbai0@icloud.com
+            feature = display_part().Features.GetParentFeatureOfBody(solid_body_layer_1)
+            feature.MakeCurrentFeature()
 
-        feature = display_part().Features.GetParentFeatureOfBody(solid_body_layer_1)
-        feature.MakeCurrentFeature()
+            displayModification1 = theSession.DisplayManager.NewDisplayModification()
+            displayModification1.ApplyToAllFaces = True
+            displayModification1.ApplyToOwningParts = False
+            displayModification1.NewColor = color
+            displayModification1.NewLayer = layer
+            displayModification1.Apply([solid_body_layer_1])
+            # theSession.UpdateManager.DoUpdate(
+            #     session_.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "m")
+            # )
+            displayModification1.Dispose()
 
-        displayModification1 = theSession.DisplayManager.NewDisplayModification()
-        displayModification1.ApplyToAllFaces = True
-        displayModification1.ApplyToOwningParts = False
-        displayModification1.NewColor = color
-        displayModification1.NewLayer = layer
-        displayModification1.Apply([solid_body_layer_1])
-        theSession.UpdateManager.DoUpdate(
-            session_.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "m")
-        )
-        displayModification1.Dispose()
+            features = list(display_part().Features)
 
-        features = list(display_part().Features)
+            features[len(features) - 1].MakeCurrentFeature()
 
-        features[len(features) - 1].MakeCurrentFeature()
+    except  Exception as ex:
+        print_(ex)
 
+    finally:
+        session_.Parts.SetDisplay(original, False, False)
+        
+    for comp in components:
+            # need to set the {comp} to the {layer} first before you change displayed part
 
-finally:
-    session_.Parts.SetDisplay(original, False, False)
-    
-for comp in components:
-        # need to set the {comp} to the {layer} first before you change displayed part
+            # foreach selected component, need to prompt the user for a layer, and color
+        comp.Layer= layer
+        comp.RedisplayObject()
 
-        # foreach selected component, need to prompt the user for a layer, and color
-
-    comp.SetLayerOption(-1)
-    comp.RedisplayObject()
+        comp.SetLayerOption(-1)
+        comp.RedisplayObject()
+        print_(f'{comp.Name}-{comp.Tag}-{comp.Color}-{comp.Layer}')
+    NXOpen.UF.UFSession.GetUFSession().Disp.RegenerateDisplay()
